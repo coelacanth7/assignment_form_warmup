628eb7501f1ff9558c90ec80fc3aa895172f3e52
Coelacanth7 <galford151@gmail.com>
2018-01-31 19:04:16 -0700

    got running without sass

---
+++ src/components/SuccessMessage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Alert } from "reactstrap";
+
+const SuccessMessage = ({ success }) => {
+	if (!success) {
+		return null;
+	}
+
+	return <Alert color="success">Your data was successfully submitted!</Alert>;
+};
+
+// SuccessMessage.propTypes = {
+//   success: PropTypes.bool,
+// }
+
+export default SuccessMessage;
